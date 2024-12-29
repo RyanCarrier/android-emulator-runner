9cc735f2e589bfd51c96590fbe7808dea2b129d3
Ryan Carrier <ryan.j.carrier@gmail.com>
2024-12-29 21:47:14 +1000

    fix: retry not working, use try catch

--- src/retry.ts
+++ src/retry.ts
@@ -3,11 +3,17 @@ export async function execWithRetry(fn: () => Promise<number>, retryCount: numbe
   let attempt = 0;
   let result = 1;
   while (attempt <= retryCount && result !== 0) {
+    console.log(`Attempt ${attempt + 1}.`);
     if (attempt > 0) {
       console.log(`Retry attempt ${attempt}. (exit code was ${result})`);
       await new Promise((resolve) => setTimeout(resolve, 1000));
     }
-    result = await fn();
+    try {
+      result = await fn();
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      result = 1;
+    }
     core.info(`Attempt ${attempt} completed with exit code ${result}`);
     attempt++;
   }
